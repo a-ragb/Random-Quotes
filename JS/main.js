cc0542be36c720e550aa30d7511280852c0c8ef5
a-ragb <a.ragab.elhassan@gmail.com>
2024-12-13 21:53:44 +0200

    First commit

--- JS/main.js
+++ JS/main.js
@@ -1,49 +1,103 @@
-var quotes_box = [
-    {
-        content: "&quot;Be yourself; everyone else is already taken.&quot;",
-        author: "--Oscar Wilde"
-    },
-    {
-        content: "&quot;So many books, so little time.&quot;",
-        author: "--Frank Zappa"
-    },
-    {
-        content: "&quot;A room without books is like a body without a soul.&quot;",
-        author: "--Marcus Tullius Cicero"
-    },
-    {
-        content: "&quot;Be the change that you wish to see in the world.&quot;",
-        author: "--Mahatma Gandhi"
-    },
-    {
-        content: "&quot;You miss 100% of the shots you don't take.&quot;",
-        author: "--Wayne Gretzy"
-    },
-    {
-        content: "&quot;Resentment is like drinking poison and waiting for your enemies to die.&quot;",
-        author: "--Nelson Mandela"
-    },
-    {
-        content: "&quot;Do not take life too seriously. You will not get out alive.&quot;",
-        author: "--Elbert Hubbard"
-    },
-    {
-        content: "&quot;The best revenge is massive success.&quot;",
-        author: "--Frank Sinatra"
-    },
-    {
-        content: "&quot;It's not what happens to you, but how you react to it that matters.&quot;",
-        author: "--Epictetus"
-    },
-    {
-        content: "&quot;Beware of what you become in pursuit of what you want.&quot;",
-        author: "--Jim Rohn"
-    }
-];
+var bookmarkName = document.querySelector("#bookmarkName");
+var bookmarkUrl = document.querySelector("#bookmarkUrl");
 
-function display_quote() {
-    var index = Math.floor(Math.random() * 10);
-    document.getElementById("quotes").innerHTML = `<p>${quotes_box[index].content}</p>
-                                                    <p>${quotes_box[index].author}</p>`
+var submitBtn = document.querySelector(".submit-button");
+var visitBtn = document.querySelector(".visit-button");
+var deleteBtn = document.querySelector(".delete-button");
+var closeBtn = document.querySelector(".close-button");
+
+var alert = document.querySelector(".alert-section");
+
+var bookmarkList = [];
+
+var regex = {
+    bookmarkName: {
+        val: /^[\sa-zA-Z0-9_-]{3,}$/,
+        isValid: false
+    },
+    bookmarkUrl: {
+        val: /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        isValid: false
+    }
 }
 
+if (localStorage.getItem("bookmarkList") != null) {
+    bookmarkList = JSON.parse(localStorage.getItem("bookmarkList"));
+    displayTable();
+}
+
+submitBtn.addEventListener("click", function () {
+    if(regex.bookmarkName.isValid && regex.bookmarkUrl.isValid){
+        alert.classList.add("d-none");
+        alert.classList.remove("d-block");
+        addBookmark();
+    }
+    else {
+        alert.classList.add("d-block");
+        alert.classList.remove("d-none");
+    }
+})
+
+closeBtn.addEventListener("click", function () {
+    alert.classList.add("d-none");
+    alert.classList.remove("d-block");
+})
+
+function addBookmark () {
+    var bookmark = {
+        name: bookmarkName.value,
+        url: bookmarkUrl.value
+    };
+
+    bookmarkList.push(bookmark);
+
+    localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+
+    clearInputs();
+
+    displayTable();
+}
+
+function displayTable () {
+    var cartona= ``;
+
+    for (var i = 0; i < bookmarkList.length; i++) {
+        cartona+=`<tr>
+                        <td>${i+1}</td>
+                        <td>${bookmarkList[i].name}</td>
+                        <td><button onclick="visitSite(${i})" class="btn visit-button"><i class="fa-solid fa-eye pe-2"></i>Visit</button></td>
+                        <td><button onclick="deleteRow(${i})" class="btn pe-2 delete-button"><i class="fa-solid fa-trash-can"></i> Delete</button></td>
+                    </tr>`;
+    }
+
+    document.querySelector(".table-body").innerHTML = cartona;
+}
+
+function clearInputs () {
+    bookmarkName.value = "";
+    bookmarkUrl.value = "";
+}
+
+function deleteRow (index) {
+    bookmarkList.splice(index,1);
+    localStorage.setItem("bookmarkList", JSON.stringify(bookmarkList));
+    displayTable();
+}
+
+function visitSite (index) {
+    window.open("https://" + bookmarkList[index].url);
+}
+
+function validateInputs (element) {  
+
+    if(regex[element.id].val.test(element.value)){
+        element.classList.add("is-valid");
+        element.classList.remove("is-invalid");
+        regex[element.id].isValid = true;
+    }
+    else {
+        element.classList.add("is-invalid");
+        element.classList.remove("is-valid");
+        regex[element.id].isValid = false;
+    }
+}
